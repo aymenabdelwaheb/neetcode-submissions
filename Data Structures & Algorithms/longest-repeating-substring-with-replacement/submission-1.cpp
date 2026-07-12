eca3096f231026769eda42e284160488793bb5a5
aymenabdelwaheb <aymenabdelwaheb19@gmail.com>
2026-07-13 00:37:37 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int res=0;
+        int maxNum=0;
+        unordered_map<char,int> freq;
+        int l=0;
+        for(int r=0;r<s.size();r++){
+            freq[s[r]]++;
+            maxNum=max(maxNum,freq[s[r]]);
+            while((r-l+1-maxNum)>k){
+                freq[s[l]]--;
+                l++;
+            }
+            
+            res=max(res,r-l+1);
+            
+        }
+        return res;
+    }
+};
